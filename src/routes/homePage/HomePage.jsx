310d6ab6c3a4090444c8a941177a5ea3e4b8cdd3
harmansaini4tech <harmansaini10000@gmail.com>
2024-11-07 15:53:44 +0530

    builded  homepage

--- src/routes/homePage/HomePage.jsx
+++ src/routes/homePage/HomePage.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
-import "../homePage/homePage.css"
+import "./homePage.css"
 import { Link } from 'react-router-dom'
 const HomePage = () => {
   return (
     <div className='homepage'>
-      <Link to="/dashboard">
-        Dashboard
-      </Link>
+      <img src="/orbital.png" alt="" className='orbital'/>
+      <div className="left">
+        <h1>GEMINI AI</h1>
+        <h2>Supercharge your creativity and productivity!</h2>
+        <h3>Lorem ipsum dolor sit amet  Nulla suscipit voluptates aspernatur repudiandae ipsam   </h3> 
+        <Link to="/dashboard">Get Started</Link>
+      </div>
+      <div className="right"></div>
     </div>
   )
 }
--- src/routes/signUpPage/SignUpPage.jsx
+++ src/routes/signUpPage/SignUpPage.jsx
@@ -1,11 +1,13 @@
-import { SignUp } from '@clerk/clerk-react'
-import React from 'react'
-import "./signUpPage.css"
+import { SignUp } from "@clerk/clerk-react";
+import React from "react";
+import "./signUpPage.css";
 
 const SignUpPage = () => {
   return (
-    <div><SignUp path="/sign-up" /></div>
-  )
-}
+    <div className="signUpPage">
+      <SignUp path="/sign-up"  signInUrl="/sign-in"/>
+    </div>
+  );
+};
 
-export default SignUpPage
+export default SignUpPage;
